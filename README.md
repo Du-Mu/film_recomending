ad7f4261245d2cae0a3ccf82c2a00ac754e67e5d
Nemo <wzhdxtx123@outlook.com>
2022-11-22 20:36:20 +0800

    [add]add analysing addr data and basic data

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# file_recomending  
+# file_recomending 
+ 
 Crawler for film critics from douban.com  
   
 # Basic Framework  
@@ -8,29 +9,54 @@ Crawler for film critics from douban.com
     - get a json file including films data, short commonts and commontator's ip address
 - NLP for analyzing  
     - analyze_data.py
-    - get the most popular key word from commonts
-
 
 data file struct:  
 
 ```bash
 --film_data
   |  |--film1_data
+  |  |  |--addr.json
+  |  |  |--basic.json
+  |  |  |--comments.json
+  |  |  |--rating.json
   |  |--film2_data
   |  ...
   |--get_data.py
   |--data_analysis.py
 ```
 
-# paper_planning
+## dependence
+- requests
+  处理web交互
+
+- bs4
+  html处理
+
+- matplotlib
+  画图处理
+
+- snowNLP
+  NLP
+
+
+
+# Paper Planning
 
 ## Abstract
 ## Modul
 ## Data Analysis
   - 影片基本信息
+    - 表格化处理
   - 地区分布
+    - 画出扇形分布图
   - 评论处理
-    - 分词
-    - 情感分析
+    - 对于每一个评论做一个分词处理:提取关键词
+    - 对于每一个评论做情感分析:细化分数
+    - 对分析做一个正态函数拟合:获取均值
+    - 统计情感分数分词频度:获取高频关键词
 ## Related Work
-## evaluation
+snowNLP
+
+## Evaluation
+对于情感分析和分数做一个准确度分析
+判断偏差值
---
+++ data_analysis.py
@@ -0,0 +1,51 @@
+import csv
+import json
+import matplotlib.pyplot as plt
+'''
+"bestRating": "10",
+        "ratingCount": "70247",
+        "ratingValue": "8.6",
+        "worstRating": "2"
+'''
+def analysis_basic_info(csv_file_name):
+    header = ['name','data','genre','data','rating_count', 'rating_value']
+    csv_file = open(csv_file_name, 'w')
+    writer = csv.writer(csv_file)
+    writer.writerow(header)
+
+    for i in range(1, 11):
+        file = open('./film_data/film'+str(i)+'/basic.json', 'r')
+        basic_info = json.load(fp=file)
+        row = [
+            basic_info['name'],
+            basic_info['datePublished'],
+            basic_info['genre'],
+            basic_info['aggregateRating']['ratingCount'],
+            basic_info['aggregateRating']['ratingValue'],
+        ]
+        writer.writerow(row)
+        file.close()
+    csv_file.close()
+
+
+def analysis_addr_info(file_name):
+    file = open(file_name, 'r')
+    addr_info = json.load(fp=file)
+    file.close()
+    
+    addr_info['else'] = 0
+    for key in list(addr_info.keys()):
+        if (addr_info[key] < 8):
+            addr_info['else'] += addr_info[key]
+            del addr_info[key]
+
+    plt.rcParams['font.sans-serif']=['KaiTi','SimHei','FangSong']
+    plt.figure(figsize = (8,8))
+    explode = []
+    for i in range(0, len(addr_info)):
+        explode.append(i*0.01)
+    plt.pie(addr_info.values(), labels=addr_info.keys(), explode = explode, autopct = '%1.2f%%',
+        pctdistance = 0.8, labeldistance = 0.9)
+    plt.title("addr_info")
+    plt.savefig('final.jpg')
+    
